2a9be03bb4198a58802612c12b91a41806ad766e
Martin Lasek <martinlasek@hotmail.de>
2018-05-19 09:33:21 +0200

    update code for latest Vapor 3 changes

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -4,13 +4,10 @@ import Vapor
 final class User: PostgreSQLModel {
     var id: Int?
     var username: String
-
     init(id: Int? = nil, username: String) {
         self.id = id
         self.username = username
     }
 }
-
 extension User: Content {}
-
 extension User: Migration {}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,24 +2,24 @@ import Vapor
 import Leaf
 import FluentPostgreSQL
 
+/// Called before your application initializes.
 public func configure(
     _ config: inout Config,
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
 
-    // Register routes to the router
+    /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
 
-    let myService = EngineServerConfig.default(port: 8006)
+    let myService = NIOServerConfig.default(port: 8006)
     services.register(myService)
 
     try services.register(LeafProvider())
     try services.register(FluentPostgreSQLProvider())
-
-    config.prefer(LeafRenderer.self, for: TemplateRenderer.self)
+    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
 
     let postgresqlConfig = PostgreSQLDatabaseConfig(
         hostname: "127.0.0.1",
@@ -27,6 +27,7 @@ public func configure(
         username: "martinlasek",
         database: "mycooldb",
         password: nil
+
     )
     services.register(postgresqlConfig)
 
